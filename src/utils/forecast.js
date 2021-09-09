a0809d176c46ccb1caeb9ffbd01f8c9f309f7669
Jose Mejia <jose.mejia@e-volution.co>
2021-09-09 11:33:31 -0500

    add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const forecast = (location, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=4043c0c9e064bcd81e85163e406bf381&query=${encodeURIComponent(location)}&units=f`;
+    const url = `http://api.weatherstack.com/current?access_key=4043c0c9e064bcd81e85163e406bf381&query=${encodeURIComponent(location)}&units=m`;
     request({ url, json: true}, ( error, { body } ) => {
         if (error) {
             callback('unable to connecto to the api', undefined);
